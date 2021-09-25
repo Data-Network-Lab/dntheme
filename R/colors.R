cc7817bd705cfb08ce1d79889d89e9c4a21bedf3
Niccolo Salvini <niccolo.salvini27@gmail.com>
2021-09-25 19:19:53 +0200

    initial commit colors

---
+++ R/colors.R
@@ -0,0 +1,11 @@
+
+pilot_colors <- c(
+  "navy" = "#204466",
+  "blue" = "#249db5",
+  "brown" = "#b84818",
+  "green" = "#30c788",
+  "yellow" = "#ffc517",
+  "purple" = "#9956db",
+  "orange" = "#f28100")
+
+
