bf8daa31dc79734e0b486dbdd9903229354c450b
Niccolo Salvini <niccolo.salvini27@gmail.com>
2021-09-26 19:45:04 +0200

    change color palette

--- R/colors.R
+++ R/colors.R
@@ -1,11 +1,26 @@
 
-pilot_colors <- c(
-  "navy" = "#204466",
-  "blue" = "#249db5",
-  "brown" = "#b84818",
-  "green" = "#30c788",
-  "yellow" = "#ffc517",
-  "purple" = "#9956db",
-  "orange" = "#f28100")
+#' Data Network colors
+#' @export
+
+dn_colors <- c(
+  "han_purple"             = "#5c3afe",
+  "maximum_blu_purple"     = "#ad9cfe",
+  "lavender_blue_1"        = "#ccc2fe",
+  "lavender_blue_2"        = "#ddd7fe",
+  "sky_blue_crayola"       = "#71d1de",
+  "turquoise_1"            = "#7ef1e0",
+  "turquoise_2"            = "#20e9c8"
+  )
 
 
+
+
+#' Get the hex code for a dn color
+#'
+#' @param color_name The name of the color.
+#'
+#' @return
+#' @export
+dn_color <- function (color_name) {
+  unname(dn_colors[color_name])
+}
