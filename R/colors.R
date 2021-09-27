65c5cf2939808a4f5e56c23c9a8657feca9811b5
Niccolo Salvini <niccolo.salvini27@gmail.com>
2021-09-27 23:05:24 +0200

    refactored code

--- R/colors.R
+++ R/colors.R
@@ -1,8 +1,8 @@
 
 #' Data Network colors
 #' @export
-
 dn_colors <- c(
+
   "han_purple"             = "#5c3afe",
   "maximum_blu_purple"     = "#ad9cfe",
   "lavender_blue_1"        = "#ccc2fe",
@@ -10,17 +10,31 @@ dn_colors <- c(
   "sky_blue_crayola"       = "#71d1de",
   "turquoise_1"            = "#7ef1e0",
   "turquoise_2"            = "#20e9c8"
-  )
+
+)
 
 
 
 
-#' Get the hex code for a dn color
+#' Get the hex code for a dn color palette
 #'
-#' @param color_name The name of the color.
+#' @param color_name the color name in dn_colors
 #'
 #' @return
 #' @export
-dn_color <- function (color_name) {
+get_dn_hex <- function (color_name) {
   unname(dn_colors[color_name])
+}
+
+
+#' Extract Data Network colors fron hex codes
+#'
+#' @param ... Names of colors in \code{dn_colors}.
+#' @export
+get_dn_colors <- function(...) {
+
+  colors <- c(...)
+  if (is.null(colors))
+    return (dn_colors)
+  dn_colors[colors]
 }
